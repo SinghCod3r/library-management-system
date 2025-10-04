f1d283cc585dbb84a747c93435085dacff18895f
Ayush Singh <ayushsinghceee@gmail.com>
2025-10-05 01:52:14 +0530

    Delete LibraryManagement directory

--- LibraryManagement/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace LibraryManagement.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        // Add additional user properties here if needed
-    }
-}
